731903d6dfeac3c27c97dd35c1c8b88d53547819
arncar123 <carilloarnold68@gmail.com>
2024-05-03 23:45:39 +0800

    Update web.php
    For prevent route access

--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\Auth\LoginController;
 
-Route::get('/', function () {
-    return view('auth.login');
-})->name('login');
-
-Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [LoginController::class, 'login']);
-
-Route::get('/posts', [PostController::class,'index'])->name('posts.index');
-Route::get('/create', function(){
-    return view('posts.create');
-})->name('posts.create');
-Route::post('/store', [PostController::class,'store'])->name('posts.store');
-Route::get('/show/{id}', [PostController::class,'show'])->name('posts.show');
-Route::get('/edit/{id}', [PostController::class,'edit'])->name('posts.edit');
-Route::put('/update/{id}', [PostController::class,'update'])->name('posts.update');
-Route::delete('/delete/{id}', [PostController::class,'destroy'])->name('posts.destroy');
-
+Route::middleware(['auth'])->group(function () {
+    Route::get('/posts', [PostController::class,'index'])->name('posts.index');
+    Route::get('/create', function(){
+        return view('posts.create');
+    })->name('posts.create');
+    Route::post('/store', [PostController::class,'store'])->name('posts.store');
+    Route::get('/show/{id}', [PostController::class,'show'])->name('posts.show');
+    Route::get('/edit/{id}', [PostController::class,'edit'])->name('posts.edit');
+    Route::put('/update/{id}', [PostController::class,'update'])->name('posts.update');
+    Route::delete('/delete/{id}', [PostController::class,'destroy'])->name('posts.destroy');
+});
