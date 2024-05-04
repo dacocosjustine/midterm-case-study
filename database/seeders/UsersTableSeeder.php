f6155621a20c301fac9032682e51ab5997a8d1c4
BalderamaLawrence <166908316+BalderamaLawrence@users.noreply.github.com>
2024-05-04 13:38:08 +0800

    Update UsersTableSeeder.php

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -10,11 +10,23 @@ class UsersTableSeeder extends Seeder
     {
         $users = [
             ['name' => 'Justine Dacocos', 'email' => 'dacocos@example.com', 'password' => Hash::make('password')],
-            ['name' => 'Admin', 'email' => 'admin@localhost', 'password' => Hash::make('password'), 'is_admin' => true],
+            ['name' => 'Admin', 'email' => 'admin@localhost', 'password' => Hash::make('password')],
             ['name' => 'User', 'email' => 'user@localhost', 'password' => Hash::make('password')],
-            ['name' => 'Moderator', 'email' => 'moderator@localhost', 'password' => Hash::make('password'), 'is_moderator' => true],
-            ['name' => 'Guest', 'email' => 'guest@localhost', 'password' => Hash::make('password'), 'is_guest' => true],
-            ['name' => 'Super User', 'email' => 'superuser@localhost', 'password' => Hash::make('password'), 'is_admin' => true, 'is_moderator' => true, 'is_guest' => true]
+            ['name' => 'Moderator', 'email' => 'moderator@localhost', 'password' => Hash::make('password')],
+            ['name' => 'Guest', 'email' => 'guest@localhost', 'password' => Hash::make('password')],
+            ['name' => 'Super User', 'email' => 'superuser@localhost', 'password' => Hash::make('password')],
+            ['name' => 'John Doe', 'email' => 'djohndoe@example.com', 'password' => Hash::make('password')],
+            ['name' => 'Jane Doe', 'email' => 'doejane@example.com', 'password' => Hash::make('password')],
+            ['name' => 'Juan Dela Cruz', 'email' => 'dcruzjuan@example.com', 'password' => Hash::make('password')],
+            ['name' => 'Juan Pablo', 'email' => 'pablojuan@example.com', 'password' => Hash::make('password')],
+            ['name' => 'Charlse Henrique', 'email' => 'henriquecharlos@example.com', 'password' => Hash::make('password')],
+            ['name' => 'Sherlock Holmes', 'email' => 'sholmes@example.com', 'password' => Hash::make('password')],
+            ['name' => 'Jeanne Baark', 'email' => 'baarkjeanne@example.com', 'password' => Hash::make('password')],
+            ['name' => 'Marie Antone', 'email' => 'antonemarie@example.com', 'password' => Hash::make('password')],
+            ['name' => 'Java Suarzez', 'email' => 'suarzez@example.com', 'password' => Hash::make('password')],
+            ['name' => 'Da Vinki', 'email' => 'dvinki@example.com', 'password' => Hash::make('password')],
+            ['name' => 'Arthur Pena', 'email' => 'penaarthur@example.com', 'password' => Hash::make('password')],
+            
         ];
 
         foreach ($users as $user) {
